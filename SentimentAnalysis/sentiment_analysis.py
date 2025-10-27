d06b3f63b5f823931821c41b6d7c323126513873
Ahmedns09 <anskhzyan@gmail.com>
2025-10-27 09:22:15 -0400

    modified the code quality based on PEP8 guidelines and tested with a result of 10/10

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -1,19 +1,28 @@
-import requests
+"""
+Provides access to the `sentiment_analyzer` function for 
+performing sentiment analysis on the given text.
+"""
+
 import json
+import requests
 
 def sentiment_analyzer(text_to_analyse):
+    '''
+    Executing this function fetches and returns the sentiment 
+    analysis score and label of the respective input, from the 
+    Watson AI NLP Library.
+    '''
     # URL of the sentiment analysis service
-    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
-    
+    url = (
+        "https://sn-watson-sentiment-bert.labs.skills.network/"
+        "v1/watson.runtime.nlp.v1/NlpService/SentimentPredict"
+    )
     # Custom header specifying the model ID for the sentiment analysis service
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
-    
     # Constructing the request payload in the expected format
     myobj = { "raw_document": { "text": text_to_analyse } }
-    
     # Sending a POST request to the sentiment analysis API
-    response = requests.post(url, json = myobj, headers=header)
-    
+    response = requests.post(url, json = myobj, headers=header, timeout=10)
     # Parsing the JSON response from the API
     formatted_response = json.loads(response.text)
 
@@ -23,6 +32,10 @@ def sentiment_analyzer(text_to_analyse):
         score = formatted_response['documentSentiment']['score']
 
     elif response.status_code == 500:
+        label = None
+        score = None
+
+    else:
         label = None
         score = None
 
