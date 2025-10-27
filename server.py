01f3fd04cabd7e71a25ffb6bd9ab238b60431c76
Ahmedns09 <anskhzyan@gmail.com>
2025-10-27 08:02:54 -0400

    added code for home page route and to execute the file in given host and port

--- server.py
+++ server.py
@@ -35,7 +35,11 @@ def render_index_page():
         page over the Flask channel
     '''
     #TODO
+    return render_template("index.html")
 
 if __name__ == "__main__":
     ''' This functions executes the flask app and deploys it on localhost:5000
-    '''#TODO
+    '''
+    #TODO
+    app.run(host="0.0.0.0", port=5000)
+
